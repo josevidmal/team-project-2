4012ecbfc7d152407ef031ba5f5147651542418f
josevidmal <92562836+josevidmal@users.noreply.github.com>
2022-02-24 21:59:00 -0600

    Adding code to userRoutes.js, homeRoutes.js and public/login.js

---
+++ public/js/login.js
@@ -0,0 +1,44 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector(/* add class or id */).value.trim();
+    const password = document.querySelector(/* add class or id */).value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert('Could not log in.');
+        }
+    }
+};
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector(/* add class or id */).value.trim();
+    const password = document.querySelector(/* add class or id */).value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert('Could not sign up.');
+        }
+    }
+};
+
+document.querySelector(/* add class or id */).value.trim();
+document.querySelector(/* add class or id */).value.trim();
