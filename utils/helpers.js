b9d2bfb1dbfac5e21899c20e779d445a377787fb
StitchCodes <coderstitch@gmail.com>
2022-02-24 20:45:30 -0600

    Format for date + time

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,10 @@
 module.exports = {
     format_date: (date) => {
-      // Format date as MM/DD/YYYY
-      return date.toLocaleDateString();
+        const scheduleDate = new Date(date);
+        const dateOptions = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+
+        // Format date as MM/DD/YYYY HH:mm
+        return scheduleDate.toLocaleDateString('en-US', dateOptions)
     }
   };
   
