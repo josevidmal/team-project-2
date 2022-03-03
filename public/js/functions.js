b8ccfeccc011460704edbc5f0ccccd6daecfdf23
Nathan Ag <nathanaguila22@gmail.com>
2022-03-02 20:05:13 -0600

    Added style sheets to correct folder for handlebars

---
+++ public/js/functions.js
@@ -0,0 +1,38 @@
+// Menu button starts here
+const menuButton = document.querySelector("#menu-button");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+menuButton.addEventListener("click", () => {
+    ul.classList.toggle("show");
+  });
+
+  // Slideshow function starts here
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  } 
