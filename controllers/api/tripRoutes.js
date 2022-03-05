6324712b3d87ea7075b61e46d70e896a21d67020
josevidmal <92562836+josevidmal@users.noreply.github.com>
2022-03-05 10:13:20 -0600

    Adding DELETE route to tripRoutes.js

--- controllers/api/tripRoutes.js
+++ controllers/api/tripRoutes.js
@@ -45,7 +45,24 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.delete('/:id', async (req, res) => {
+  try {
+    const tripData = await Trips.destroy({
+      where: {
+        trip_id: req.params.id,
+      },
+    });
 
+    if (!tripData) {
+      res.status(404).json({ message: "No Trip found with that id!" });
+      return;
+    }
+
+    res.status(200).json(tripData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 // MODULE EXPORT
